9e9d75308161cc3a41b9a45f252a80e5ad58d097
leotgit <118315225+leotgit@users.noreply.github.com>
2022-12-13 20:48:39 -0300

    Add files via upload

---
+++ funcion.js
@@ -0,0 +1,9 @@
+let edad;
+
+edad = prompt("¿Cuántos años tenés?");
+
+if (edad >= 18)
+{alert("Podés entrar, sos mayor de edad.");}
+else {alert("Sos menor de edad, entrás bajo tu responsabilidad.");}
+
+
