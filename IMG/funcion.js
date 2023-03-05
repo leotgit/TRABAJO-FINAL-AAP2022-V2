193038983d4469c72927583ea92b516c06eccc8b
leotgit <118315225+leotgit@users.noreply.github.com>
2023-03-05 18:42:49 -0300

    Delete IMG directory

--- IMG/funcion.js
+++
@@ -1,9 +0,0 @@
-let edad;
-
-edad = prompt("¿Cuántos años tenés?");
-
-if (edad >= 18)
-{alert("Podés entrar, sos mayor de edad.");}
-else {alert("Sos menor de edad, entrás bajo tu responsabilidad.");}
-
-
